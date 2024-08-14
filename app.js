28a732254cd9d4c53178a8972f199372610c8884
Shrawan2002 <shrawankumar1754@gmail.com>
2024-08-14 16:57:03 +0530

    initial Files

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const BASE_URL =
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 let btn = document.querySelector("form button");
+let fromCurr = document.querySelector(".from select");
+let tocurr = document.querySelector(".to select");
+// console.log(fromCurr);
 
 for(let select of dropdowns){
-    for(currCode in countryList){
+    for(let currCode in countryList){
         let newOption = document.createElement("option");
         newOption.innerText = currCode;
         newOption.value = currCode;
@@ -23,12 +26,28 @@ for(let select of dropdowns){
 
 
 let UpdateFlag = (element)=>{
-    console.log(element);
+    // console.log(element);
     let currCode = element.value;
-    console.log(currCode);
+    // console.log(currCode);
     let countryCode = countryList[currCode];
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
     let img =element.parentElement.querySelector("img");
     img.src = newSrc;
 
 }
+
+btn.addEventListener("click",(evt)=>{
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    // console.log(amtVal);
+    if(amtVal==""|| amtVal<1){
+        amtVal=1;
+        amount.value = "1"; // Any change to amount.value immediately reflects in the user
+        // interface (UI), meaning the value in the input field on the webpage will update instantly.
+    }
+    console.log(fromCurr.value);
+    console.log(tocurr.value);
+    
+
+})
